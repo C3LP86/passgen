1fedb5c4ebc5d58414095c5d0e6a1dac52634e8a
C3LP86 <128266832+C3LP86@users.noreply.github.com>
2023-06-22 16:18:59 +0200

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -4,8 +4,6 @@ import string
 import hashlib
 import pyfiglet
 
-
-
 print("")
 print("<====================================================================================>")
 
@@ -15,25 +13,19 @@ print(  ascii_banner  )
 print("<====================================================================================>")
 print("")
 
-
-
 parser = argparse.ArgumentParser(description='Password Generator + SHA-256 and SHA-3 Encryption')
 
 parser.add_argument("-pG", "--passwordGeneration", action="store_true", help="Password Generation")
-parser.add_argument("-sG", "--wordlistGeneration", action="store_true", help="Password generation for wordlist creation")
+parser.add_argument("-wG", "--wordlistGeneration", action="store_true", help="Password generation for wordlist creation")
 parser.add_argument("-eP", "--encryptPassword", action="store_true", help="Encryption of your own password")
 parser.add_argument("-gEP", "--generateEncryptedPassword", action="store_true", help="Password Generation + Encryption")
 parser.add_argument("-fT", "--fileTransfer", action="store_true", help="Transfer of the content to a file")
 
 args = parser.parse_args()
 
-
-
 if hasattr(args, 'help'):
     parser.print_help()
     exit()
-
-
 
 if args.passwordGeneration:
 
@@ -57,8 +49,6 @@ if args.passwordGeneration:
 			passwords += f"[{n}] - " "Here's the password : " + password + "\n"
 			n += 1
 
-		print(passwords)
-
 	else:
 		while x < 12:
 			print("You must have at least 12 characters !")
@@ -78,51 +68,37 @@ if args.passwordGeneration:
 			passwords += f"[{n}] - " "Here's the password : " + password + "\n"
 			n += 1
 
+	transfer = args.fileTransfer
+
+	if transfer:
+		print("The Content is add to your file")
+	else:
 		print(passwords)
-
-
 
 if args.wordlistGeneration:
 
 	caractères = string.ascii_letters + string.digits + string.punctuation
 
-	x = int(input("Indicates the number of characters required (minimum -> 12) : "))
+	x = int(input("Indicates the number of characters required : "))
+	y = int(input("Specifies the number of passwords required : "))
+		
+	def Generator_Password():
+		password = ""
+		for i in range(x):
+			password += random.choice(caractères)
+		return password
 
-	if x >= 12:
-		y = int(input("Specifies the number of passwords required : "))
-		def Generator_Password():
-			password = ""
-			for i in range(x):
-				password += random.choice(caractères)
-			return password
+	passwords = ""
+	for j in range(y):
+		password = Generator_Password()
+		passwords += password + "\n"
 
-		passwords = ""
-		for j in range(y):
-			password = Generator_Password()
-			passwords += password + "\n"
-			
-		print(passwords)
+	transfer = args.fileTransfer
 
+	if transfer:
+		print("The Content is add to your file")
 	else:
-		while x < 12:
-			print("You must have at least 12 characters !")
-			x = int(input("Indicates the number of characters required (minimum -> 12) : "))
-
-		y = int(input("Indicates the number of passwords required : "))
-		def Generator_Password():
-			password = ""
-			for i in range(x):
-				password += random.choice(caractères)
-			return password
-
-		passwords = ""
-		for j in range(y):
-			password = Generator_Password()
-			passwords += password + "\n"
-			
 		print(passwords)
-
-
 
 if args.encryptPassword:
 	
@@ -145,9 +121,12 @@ if args.encryptPassword:
 
 	passwords += "The encrypted password is : " + encryption + "\n" 
 
-	print(passwords)
+	transfer = args.fileTransfer
 
-
+	if transfer:
+		print("The Content is add to your file")
+	else:
+		print(passwords)
 
 if args.generateEncryptedPassword:
 
@@ -193,11 +172,14 @@ if args.generateEncryptedPassword:
 		hash_sha3 = hash_object_sha3.digest()
 		encryption = hash_sha3.hex()
 
-		passwords += f"[{j+1}] - The password '{password}' encrypted in SHA256 / SHA3 right here : {encryption}\n"
+		passwords += f"[{j+1}] - The password '{password}' encrypted in SHA256 / SHA3 right here : {encryption}\n" 
 
-	print(passwords)
+	transfer = args.fileTransfer
 
-
+	if transfer:
+		print("The Content is add to your file")
+	else:
+		print(passwords)
 
 if args.fileTransfer:
 	file = input("Enter the name of the file where you want to place the result : ")
